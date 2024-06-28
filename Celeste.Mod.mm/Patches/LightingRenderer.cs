b4eb410d93d855afbf1228190f11b1c1f62e3891
cuttestkittensrule <mangoiscute95@gmail.com>
2024-06-27 22:02:53 -0700

    Still having decomp issues, probably works

--- Celeste.Mod.mm/Patches/LightingRenderer.cs
+++ Celeste.Mod.mm/Patches/LightingRenderer.cs
@@ -6,6 +6,7 @@ using MonoMod.InlineRT;
 using MonoMod.Utils;
 using Mono.Cecil.Cil;
 using Mono.Cecil.Rocks;
+using System.Linq;
 
 namespace Celeste {
     public class patch_LightingRenderer : LightingRenderer {
@@ -18,66 +19,66 @@ namespace Celeste {
 
         [MonoModIgnore]
         [PatchLightingRendererRender]
-        public extern void Render();
+        public extern void BeforeRender();
     }
 }
 
 namespace MonoMod {
-    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchLightingRendererRender))]
+    [MonoModCustomMethodAttribute(nameof(MonoModRules.PatchLightingRendererBeforeRender))]
     class PatchLightingRendererRenderAttribute : Attribute { }
     partial class MonoModRules {
-        public static void PatchLightingRendererRender(ILContext context, CustomAttribute attrib) {
+        public static void PatchLightingRendererBeforeRender(ILContext context, CustomAttribute attrib) {
+            // References for Matching IL
+            TypeDefinition t_VertexLight = MonoModRule.Modder.FindType("Celeste.VertexLight").Resolve();
+            FieldReference f_VertexLight_Index = t_VertexLight.FindField("Index");
+
+            // References for resizing array
             TypeDefinition t_Array = MonoModRule.Modder.FindType("System.Array").Resolve();
-            TypeReference t_Edge = MonoModRule.Modder.FindType("Celeste.LightningRenderer.Edge").Resolve();
-            TypeReference t_Enumerator = MonoModRule.Modder.FindType("System.Collections.Generic.List.Enumerator").MakeGenericInstanceType(t_Edge).Resolve();
-
             MethodDefinition m_Array_Resize = t_Array.FindMethod("Resize");
+            FieldReference f_lights = context.Method.DeclaringType.FindField("lights").Resolve();
 
-            FieldReference f_edgeVerts = context.Method.DeclaringType.FindField("edgeVerts").Resolve();
+            // References for getting the level set
             FieldReference f_Session = context.Method.DeclaringType.FindField("Session").Resolve();
             FieldReference f_Session_Area = f_Session.FieldType.Resolve().FindField("Area").Resolve();
             MethodDefinition m_Area_GetLevelSet = f_Session_Area.FieldType.Resolve().FindMethod("System.String GetLevelSet()");
-            MethodDefinition get_Enumerator_Current = t_Enumerator.Resolve().FindMethod("get_Current").Resolve();
             
 
             ILCursor cursor = new ILCursor(context);
 
-            cursor.GotoNext(
-                MoveType.Before,
-                instr => instr.MatchLdloca(6),
-                instr => instr.MatchCall(get_Enumerator_Current),
-                instr => instr.MatchStloc(7));
+            int loc_component = -1;
+
+            cursor.GotoNext(MoveType.Before,
+                instr => instr.MatchLdarg0(),
+                instr => instr.MatchLdfld(f_lights),
+                instr => instr.MatchLdloc(out loc_component),
+                instr => instr.MatchLdfld(f_VertexLight_Index),
+                instr => instr.MatchLdnull(),
+                instr => instr.MatchStelemRef());
+            
 
             ILLabel end = cursor.DefineLabel();
-            // if (Session.Area.GetLevelSet() != "Celeste" && edgeVerts.length - index <= 16) {
+            // if (Session.Area.GetLevelSet() != "Celeste" && edgeVerts.length >= component.Index) {
             // Session.Area.GetLevelSet() != "Celeste"
             cursor.EmitLdarg0();
-            cursor.Emit(OpCodes.Ldfld, f_Session);
-            cursor.Emit(OpCodes.Ldfld, f_Session_Area);
+            cursor.EmitLdfld(f_Session);
+            cursor.EmitLdfld(f_Session_Area);
             cursor.EmitCall(m_Area_GetLevelSet);
             cursor.EmitLdstr("Celeste");
-            cursor.EmitCeq();
-            cursor.EmitNot();
+            cursor.EmitBeq(end);
 
-            // edgeVerts.length - index <= 16
+            // lights.length >= component.Index
             cursor.EmitLdarg0();
-            cursor.Emit(OpCodes.Ldfld, f_edgeVerts);
+            cursor.EmitLdfld(f_lights);
             cursor.EmitLdlen();
-            cursor.EmitLdloc3();
-            cursor.EmitSub();
-            cursor.EmitLdcI4(16);
-            cursor.EmitCgt();
+            cursor.EmitLdloc(loc_component);
+            cursor.EmitLdfld(f_VertexLight_Index);
+            cursor.EmitBlt(end);
 
-            //(... && ...) {
-            cursor.EmitAnd();
-            cursor.EmitBrfalse(end);
-
-            // Array.Resize(ref edgeVerts, edgeVerts.length + 32);
-            cursor.EmitLdloca(3);
-            cursor.EmitLdarg0();
-            cursor.Emit(OpCodes.Ldfld, f_edgeVerts);
-            cursor.EmitLdlen();
-            cursor.EmitLdcI4(32);
+            // Array.Resize(ref lights, component.Index + 1);
+            cursor.EmitLdflda(f_lights);
+            cursor.EmitLdloc(loc_component);
+            cursor.EmitLdfld(f_VertexLight_Index);
+            cursor.EmitLdcI4(1);
             cursor.EmitAddOvf();
             cursor.EmitCall(m_Array_Resize);
 
